f680b97d15920159ca172a2bdc550cabd93208be
CarlosEduDev <duducarlos1327@gmail.com>
2022-08-07 14:25:26 -0300

    Novo teste

--- app/src/main/java/com/easyapp/simpletask/demo/MainActivity.java
+++ app/src/main/java/com/easyapp/simpletask/demo/MainActivity.java
@@ -2,6 +2,7 @@ package com.easyapp.simpletask.demo;
  
 import android.app.Activity;
 import android.os.Bundle;
+import com.easyapp.simpletask.SimpleTask;
 
 public class MainActivity extends Activity { 
      
@@ -9,6 +10,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+      
         
     }
 	
